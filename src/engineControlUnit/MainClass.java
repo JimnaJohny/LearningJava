d09dbcbe90e362b8855f8c24efc4f845099fe46f
Jimna Johny <jimnajohny@gmail.com>
2023-11-09 21:38:14 -0500

    Added JIMNA

--- src/engineControlUnit/MainClass.java
+++ src/engineControlUnit/MainClass.java
@@ -1,11 +1,13 @@
 package engineControlUnit;
-
+import java.util.Scanner;
 public class MainClass {
 
 	public static void main(String[] args) {
 		CalculateFuelInjection calculateFuelInjection = new CalculateFuelInjection();
 		calculateFuelInjection.xThrottle = 500;
 		calculateFuelInjection.calculateFuelInjection();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("JIMNA");
 	}
 
 }
